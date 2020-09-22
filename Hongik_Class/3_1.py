dfaaa135e5a7e38145caefb165c3fbce60e72d29
kacias <kacias@daum.net>
2020-09-22 19:03:37 +0900

    Update 3_1.py

--- Hongik_Class/3_1.py
+++ Hongik_Class/3_1.py
@@ -1,6 +1,20 @@
 from typing import List
 
 
+'''
+처음에 문제를 이해하는 데 정말 오래 걸렸다. 지금도 정확히 이해한 것인지 모르겠지만 문제를 통과한 것으로 보면 맞게 이해한 것으로 생각된다.
+
+주어지는 input에는 두 가지 특성의 데이터가 존재한다. 하나는 'letter-logs'이고 다른 하나는 'digit-log'이다. 그리고 output은 다음과 같은 제약조건을 가진다.
+
+logs의 각 인자의 첫번째 단어를 identifier로 칭한다.
+letter-log는 digit-log보다 먼저 정렬되어야 한다.
+letter-log는 identifier을 무시하고 다음 단어부터 사전식 정렬순을 가진다. 만약 사전식 정렬이 같을 경우 identifier를 고려한다.
+digit-log는 input과 같은 순을 가진다.
+그렇기 때문에 input에서 letter-log와 digit-log를 구분해야 했으며, 첫번째 단어의 글자의 형식(숫자인지 문자인지, isnumeric())에 따라 둘을 구분하였다. 그리고 letter-log의 순서는 먼저 첫번째 단어(identifier) 이후의 단어로 정렬하고 그 다음 첫번째 단어(identifier)로 정렬해서 digit-log와 합친 후(순서대로 append 되었다) 결과를 구한다.
+
+'''
+
+
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         letters, digits = [], []
@@ -15,7 +29,37 @@ class Solution:
         return letters + digits
 
 if __name__ == "__main__":
+
+    #split()
+    '''
+    a = "naver daum"
+    b = a.split()
+    print(b)
+
+
+    a = "naver_daum"
+    b = a.split("_")
+    print(b)
+
+    a = "naver daum"
+    b = a.split()[0]
+    c = a.split()[1]
+    print(b)
+    print(c)
+    '''
+
+    #isdigit()
+    '''
+    e="111"
+    f="abc"
+    print(e.isdigit())
+    print(f.isalpha())
+    '''
+
+
+    '''
     a = Solution()
     b = a.reorderLogFiles("skdjl ldkjfk")
 
     print(b)
+    '''
