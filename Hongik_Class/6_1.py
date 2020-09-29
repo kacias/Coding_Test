b0fb19238aec8832ef36f7e60e360a88a54e0e32
kacias <kacias@daum.net>
2020-09-29 18:12:09 +0900

    Update 6_1.py

--- Hongik_Class/6_1.py
+++ Hongik_Class/6_1.py
@@ -61,22 +61,51 @@ class Solution:
 
 if __name__ == "__main__":
 
+    #==============
+    #숫자일 경우 가장 큰 값
     '''
     a = [1,2,3,4,10]
     print(max(a))
-    '''
+
+
+    #===========================
+    #문자열 리스트일 경우 맨 "앞글자"를 기준으로 알파벳 순 정렬로 가장 뒤에 있는 거
+    c = ["Python", "Z Programming", "Java", "JavaScript", "Za"]
+    d = max(c)
+    print(d)
+
+    #------------------
+    #키 값을 Len 지정하면 가장 긴 문자열 반환
+    f = max(c, key=len)
+    print(f)
+
+    #------------------------------
+    #여러개 숫자 경우 가장 큰 값
+    result = max(4, -5, 23, 5)
+    print("The maximum number is:", result)
+
+    #-------------------------------
+    #람다함수 사용
+    names = ['Suh', 'Adrian', 'Bill', 'Jonathan']
+    longest = max(names, key= lambda n: len(n))
+    print(longest)
+
+
+    #=================================
+    #두 개 이상의 리스트 값 비교
 
     a = ["11134", "22323", "32", "sdf4", "10"]
-    b = ["1sdfsdf34", "sdgs323", "3werewr2", "swerwerwdf4", "werwer10"]
-
-
+    b = ["1sdfsdf34", "sdgs323", "0werewr2", "0werwerwdf4", "werwer10"]
+    
+    print(max(a, key=len))
+    print(max(b, key=len))
     print(max(a, b, key=len))
-
-
-
     '''
+
+
+
     a = Solution()
-    b = a.longestPalindrome(["1221", "11111111", "tomato", "apppppa"])
+    b = a.longestPalindrome(["2", "12221", "tomato", "appppa", "0000000000000"])
     print("final result:{}".format(b))
-    '''
+
 
