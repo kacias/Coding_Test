84c73d5bc5e8370847b430d1baa0d27e3a30bfcc
kacias <kacias@daum.net>
2020-10-20 18:03:17 +0900

    update

---
+++ Hongik_Class/23_1.py
@@ -0,0 +1,29 @@
+import collections
+
+
+class MyStack:
+    def __init__(self):
+        self.q = collections.deque()
+
+    def push(self, x):
+        self.q.append(x)
+        # 요소 삽입 후 맨 앞에 두는 상태로 재정렬
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self):
+        return self.q.popleft()
+
+    def top(self):
+        return self.q[0]
+
+    def empty(self):
+        return len(self.q) == 0
+
+if __name__=="__main__":
+
+    a = MyStack()
+    a.push(10)
+    a.push(20)
+    b = a.pop()
+    print(b)
---
+++ Hongik_Class/24_1.py
@@ -0,0 +1,31 @@
+class MyQueue:
+    def __init__(self):
+        self.input = []
+        self.output = []
+
+    def push(self, x):
+        self.input.append(x)
+
+    def pop(self):
+        self.peek()
+        return self.output.pop()
+
+    def peek(self):
+        # output이 없으면 모두 재입력
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+        return self.output[-1]
+
+    def empty(self):
+        return self.input == [] and self.output == []
+
+
+if __name__=="__main__":
+
+    a = MyQueue()
+    a.push(10)
+    a.push(20)
+    b = a.pop()
+    print(b)
+
