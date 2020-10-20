838fdc6a6939fef9c2fbff14718c82dec3554c5b
kacias <kacias@daum.net>
2020-10-20 17:51:38 +0900

    udpate

---
+++ Hongik_Class/20_1.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def isValid(self, s: str) -> bool:
+        stack = []
+        table = {
+            ')': '(',
+            '}': '{',
+            ']': '[',
+        }
+
+        # 스택 이용 예외 처리 및 일치 여부 판별
+        for char in s:
+            if char not in table:
+                stack.append(char)
+            elif not stack or table[char] != stack.pop():
+                return False
+        return len(stack) == 0
+
+#======================================
+#메인 함수
+
+if __name__=="__main__":
+
+    #-----------------------
+    s= "()[]{}"
+    a = Solution()
+    result = a.isValid(s)
+    print(result)
+
