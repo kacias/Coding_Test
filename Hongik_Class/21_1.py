7341f595763aad153925cb1296dcaf354d7f33ed
kacias <kacias@daum.net>
2020-10-21 12:05:21 +0900

    debuging code

--- Hongik_Class/21_1.py
+++ Hongik_Class/21_1.py
@@ -8,11 +8,55 @@ class Solution:
                 return char + self.removeDuplicateLetters(suffix.replace(char, ''))
         return ''
 
+#======================================================
+#디버깅 코드
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        # 집합으로 정렬
+        for char in sorted(set(s)):
+            print("====================")
+            print("input_s:{}".format(s))
+            print("char:{}".format(char))
+            suffix = s[s.index(char):]
+            print("suffix:{}".format(suffix))
+            # 전체 집합과 접미사 집합이 일치할때 분리 진행
+            if set(s) == set(suffix):
+                print("set:{} == set_suffix:{}".format(set(s), set(suffix)))
+                print("go deeper! char is removed")
+                return char + self.removeDuplicateLetters(suffix.replace(char, ''))
+        return ''
+
+
 
 if __name__=="__main__":
 
+    #=====================
+    #set 집합 문법 연습
+    #딕셔너리  {"a": 1, "b": 2}
+    #셋 {'a', 'b'}
+
+    set1 = {5,7,10}
+    print(set1)
+    set1.add(100)
+    set1.remove(5)
+    set1.discard(77)
+    print(set1)
+
+    set2 = set([1,4,5])
+    print(set2)
+
+    s = "abcdefff"
+    print(set(s))
+    print(sorted(set(s)))
+
+    #==========================
+    #문자열 내에서 특정 char 인덱스 값
+    print(s.index("c")) #해당 character가 있는 index 값 반환
+    print(s[s.index("c"):]) #해당 character 있는 index 이후 문자열 반환
+
+
     #-----------------------
-    s= "bcabc"
+    s= "abcabcee"
     a = Solution()
     result = a.removeDuplicateLetters(s)
     print(result)
--- Hongik_Class/21_2.py
+++ Hongik_Class/21_2.py
@@ -1,6 +1,6 @@
 import collections
 
-
+'''
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         counter, seen, stack = collections.Counter(s), set(), []
@@ -16,12 +16,57 @@ class Solution:
             seen.add(char)
 
         return ''.join(stack)
+'''
+
+#디버깅
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        counter, seen, stack = collections.Counter(s), set(), []
+
+        for char in s:
+            print("==================")
+            print("input_s:{}".format(s))
+            print("char:{}".format(char))
+            print("counter[char]:{}".format(counter[char]))
+
+            counter[char] -= 1
+            if char in seen:
+                continue
+            # 뒤에 붙일 문자가 남아 있다면 스택에서 제거
+            while stack and char < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+            stack.append(char)
+            seen.add(char)
+
+        return ''.join(stack)
+
+
 
     
 if __name__=="__main__":
 
+
+    d = "aabbcd"
+    print(collections.Counter(d))
+    e= [1,2,4,2,3,1]
+    print(collections.Counter(e))
+    f = "kangkangkang"
+    print(collections.Counter(f))
+    
+    g = {5,6,3,3,3}
+    print(collections.Counter(g))
+    f= {"a":1, "a":1, "c":5}
+    print(collections.Counter(f))
+
+
+
+
+
+
     #-----------------------
+    '''
     s= "bcabc"
     a = Solution()
     result = a.removeDuplicateLetters(s)
     print(result)
+    '''
