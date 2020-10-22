2f76d22e26bcb40c9d19d6165dc56b6cd25e1572
kacias <53470217+kacias@users.noreply.github.com>
2020-10-22 22:18:03 +0900

    update

--- Hongik_Class/21_2.py
+++ Hongik_Class/21_2.py
@@ -57,7 +57,7 @@ class Solution:
     
 if __name__=="__main__":
 
-    '''
+
     d = "aabbcd"
     print(collections.Counter(d))
     e= [1,2,4,2,3,1]
@@ -69,23 +69,16 @@ if __name__=="__main__":
     print(collections.Counter(g))
     f= {"a":1, "a":1, "c":5}
     print(collections.Counter(f))
-    '''
 
-    '''
     g = [1,2,5,10]
     print(g[-1])
     print(g)
-    '''
 
-    '''
-    #문자들간의 크기 비교는 아스키값 기준 
+    #문자들간의 크기 비교는 아스키값 기준
     l = ["a", "b", "c", "c"]
     for c in l:
         if c < l[-1]:
             print("confirm:{}".format(c))
-
-    '''
-
 
     #-----------------------
 
