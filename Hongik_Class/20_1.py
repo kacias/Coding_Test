9cf6cb0931b9c2de83a2109c5d95c32d201bae30
kacias <53470217+kacias@users.noreply.github.com>
2020-10-23 09:51:20 +0900

    Update 20_1.py

--- Hongik_Class/20_1.py
+++ Hongik_Class/20_1.py
@@ -1,3 +1,4 @@
+'''
 class Solution:
 
     def isValid(self, s: str) -> bool:
@@ -15,6 +16,44 @@ class Solution:
             elif not stack or table[char] != stack.pop():
                 return False
         return len(stack) == 0
+'''
+
+#디버깅
+class Solution:
+
+    def isValid(self, s: str) -> bool:
+        stack = []
+        table = {
+            ')': '(',
+            '}': '{',
+            ']': '[',
+        }
+
+        print("input:{}".format(s))
+
+        # 스택 이용 예외 처리 및 일치 여부 판별
+        for char in s:
+            print("==========================")
+            print("char:{}".format(char))
+            print("table_before:{}".format(table))
+            print("stack_before:{}".format(stack))
+
+            if char not in table:
+                print("pass1")
+                stack.append(char)
+                print("stack added:{}".format(stack))
+
+            elif not stack or table[char] != stack.pop():
+                print("pass2")
+                return False
+
+            print("stack_after:{}".format(stack))
+
+
+        return len(stack) == 0
+
+
+
 
 #======================================
 #메인 함수
