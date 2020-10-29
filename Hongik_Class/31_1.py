5a53f22881c2529d7287f10db7c31be4e526bcdc
kacias <kacias@daum.net>
2020-10-29 17:42:41 +0900

    Update 31_1.py

--- Hongik_Class/31_1.py
+++ Hongik_Class/31_1.py
@@ -23,21 +23,33 @@ class Solution:
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
         freqs = collections.Counter(nums)
         freqs_heap = []
         print("freqs:{}".format(freqs))
+
+        #========================
+        #파이썬에는 Max Heap이 없어서 이렇게 변형해서 사용함
         # 힙에 음수로 삽입
         for f in freqs:
             print("--------------------------------")
             print("f:{}".format(f))
             print("-freqs[f]:{}".format(-freqs[f]))
-            heapq.heappush(freqs_heap, (-freqs[f], f))
-            print("heapq:".format(heapq))
+            heapq.heappush(freqs_heap, (-freqs[f], f))  #튜플 데이터를 heap에 삽입
 
+            print("heapq:{}".format(freqs_heap))
+
+        print("final heapq:{}".format(freqs_heap))
         topk = list()
+        print("k:{}".format(k))
+
+        #=============================================
         # k번 만큼 추출, 민 힙 이므로 가장 작은 음수 순으로 추출
         for _ in range(k):
-            topk.append(heapq.heappop(freqs_heap)[1])
+            d = heapq.heappop(freqs_heap)[1]
+            print("popped data:{}".format(d))
+            topk.append(d)
+            #topk.append(heapq.heappop(freqs_heap)[1])
 
         return topk
 
@@ -47,6 +59,7 @@ if __name__=="__main__":
 
     #----------------------------
     #min heap test
+    '''
     heap =[]
 
     heapq.heappush(heap, 4)
@@ -59,21 +72,45 @@ if __name__=="__main__":
 
     #이진 트리 생성
     print(heap)
+    '''
 
     #가장 작은 값을 삭제
+    '''
     heapq.heappop(heap)
-
     print(heap)
     print(heap[0])
+    '''
 
+
+    '''
     l = [1,2,3,5,3,9]
     heapq.heapify(l)
     print(l)
     #-----------------------------
-    
+    '''
+
+    #---------------------------------
+    #우선 순위 min heap tree, 우선 순위를 튜플로 입력
+    '''
+    h = []
+    heapq.heappush(h, (3, "Go to home"))
+    heapq.heappush(h, (10, "Do not study"))
+    heapq.heappush(h, (1, "Enjoy!"))
+    heapq.heappush(h, (4, "Eat!"))
+    heapq.heappush(h, (7, "Pray!"))
+
+    print(h)
+
+    k = len(h)
+
+    #우선 순위대로 팝
+    for _ in range(k):
+        print("popped data:{}".format(heapq.heappop(h)))
+    '''
+
 
     '''
-    s= [1,2,2,3,3,3]
+    s= [1,2,2,2,2,9,9,9]
     a = Solution()
     print(a.topKFrequent(s, 2))
     '''
